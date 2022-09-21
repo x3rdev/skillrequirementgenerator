392feb2af10b48a5b02d1f1f469094f83ab5c931
X3RMINATION <nicolaeungur@gmail.com>
2022-09-21 14:06:26 -0700

    Fixed crash with null values, should ignore item instead of crashing

--- src/main/java/com/github/x3rmination/skillrequirementgenerator/mixin/ReSkillableConfigMixin.java
+++ src/main/java/com/github/x3rmination/skillrequirementgenerator/mixin/ReSkillableConfigMixin.java
@@ -20,39 +20,42 @@ import java.util.Map;
 import java.util.Objects;
 
 @Mixin(Configuration.class)
-public class ReSkillableConfigMixin {
+public abstract class ReSkillableConfigMixin {
 
     @Shadow(remap = false) @Final private static Map<String, Requirement[]> skillLocks;
 
-    @Inject(method = "load", at = @At("HEAD"), remap = false)
+    @Inject(method = "load", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;iterator()Ljava/util/Iterator;", shift = At.Shift.AFTER), remap = false)
     private static void loadMixin(CallbackInfo ci) {
         ForgeRegistries.ITEMS.getEntries().forEach(entry -> {
-            String registryName = Objects.requireNonNull(entry.getValue().getRegistryName()).toString();
-            ItemStack stack = entry.getValue().getDefaultInstance();
-            long damage = Math.round(stack.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().mapToDouble(AttributeModifier::getAmount).sum());
-            if(damage > 0) {
-                int level = (int) Math.min((Math.pow(damage, 1.5))/2F, 32);
-                if(level > 0) {
-                    Requirement[] requirements = new Requirement[1];
-                    requirements[0] = new Requirement(Skill.ATTACK, level);
-                    skillLocks.put(registryName, requirements);
-                }
-            }
-            int armor = 0;
-            for(EquipmentSlotType type : EquipmentSlotType.values()) {
-                if (type.getType().equals(EquipmentSlotType.Group.ARMOR)) {
-                    armor = (int) Math.round(stack.getAttributeModifiers(type).get(Attributes.ARMOR).stream().mapToDouble(AttributeModifier::getAmount).sum());
-                    if (armor > 0) {
-                        break;
+            Skillrequirementgenerator.LOGGER.info(entry.getValue());
+            if(entry.getValue().getRegistryName() != null) {
+                String registryName = String.valueOf(entry.getValue().getRegistryName());
+                ItemStack stack = entry.getValue().getDefaultInstance();
+                long damage = Math.round(stack.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().mapToDouble(AttributeModifier::getAmount).sum());
+                if (damage > 0) {
+                    int level = (int) Math.min((Math.pow(damage, 1.5)) / 2F, 32);
+                    if (level > 0) {
+                        Requirement[] requirements = new Requirement[1];
+                        requirements[0] = new Requirement(Skill.ATTACK, level);
+                        skillLocks.put(registryName, requirements);
                     }
                 }
-            }
-            if(armor > 0) {
-                int level = (int) Math.min((Math.pow(armor, 1.5))/2F, 32);
-                if(level > 0) {
-                    Requirement[] requirements = new Requirement[1];
-                    requirements[0] = new Requirement(Skill.DEFENCE, level);
-                    skillLocks.put(registryName, requirements);
+                int armor = 0;
+                for (EquipmentSlotType type : EquipmentSlotType.values()) {
+                    if (type.getType().equals(EquipmentSlotType.Group.ARMOR)) {
+                        armor = (int) Math.round(stack.getAttributeModifiers(type).get(Attributes.ARMOR).stream().mapToDouble(AttributeModifier::getAmount).sum());
+                        if (armor > 0) {
+                            break;
+                        }
+                    }
+                }
+                if (armor > 0) {
+                    int level = (int) Math.min((Math.pow(armor, 1.5)) / 2F, 32);
+                    if (level > 0) {
+                        Requirement[] requirements = new Requirement[1];
+                        requirements[0] = new Requirement(Skill.DEFENCE, level);
+                        skillLocks.put(registryName, requirements);
+                    }
                 }
             }
         });
