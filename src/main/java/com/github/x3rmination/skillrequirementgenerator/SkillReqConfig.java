1ae09c2e211c328a0e0a79de3a4bc664336d1944
X3RMINATION <nicolaeungur@gmail.com>
2022-12-20 19:41:44 -0800

    Switched to rereskillable-rereforked, fixed crashing, fixed config

--- src/main/java/com/github/x3rmination/skillrequirementgenerator/SkillReqConfig.java
+++ src/main/java/com/github/x3rmination/skillrequirementgenerator/SkillReqConfig.java
@@ -7,27 +7,20 @@ public class SkillReqConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.ConfigValue<Integer> max_level;
-    public static final ForgeConfigSpec.ConfigValue<Float> damage_divisor;
-    public static final ForgeConfigSpec.ConfigValue<Float> damage_power;
-    public static final ForgeConfigSpec.ConfigValue<Float> armor_divisor;
-    public static final ForgeConfigSpec.ConfigValue<Float> armor_power;
+    public static final ForgeConfigSpec.IntValue max_level;
+    public static final ForgeConfigSpec.DoubleValue damage_divisor;
+    public static final ForgeConfigSpec.DoubleValue damage_power;
+    public static final ForgeConfigSpec.DoubleValue armor_divisor;
+    public static final ForgeConfigSpec.DoubleValue armor_power;
 
 
     static {
-        BUILDER.push("Skill Requirement Generator Config");
-
-        BUILDER.comment("Formula for skill: s^p / a");
-        BUILDER.comment("s: Stat (ex. attack dmg, armor) ");
-        BUILDER.comment("p: Power of ... ");
-        BUILDER.comment("d: divisor ");
-
-        max_level = BUILDER.comment("Max level to generate (has to be less than or equal to reskillable config)").define("max_level", 32);
-        damage_power = BUILDER.comment("Damage power").define("damage_power", 2F);
-        damage_divisor = BUILDER.comment("Damage divisor").define("damage_divisor", 2F);
-        armor_power = BUILDER.comment("Armor power").define("armor_power", 2F);
-        armor_divisor = BUILDER.comment("Armor divisor").define("armor_divisor", 2F);
-
+        BUILDER.comment("Formula for skill level: s^a / b", "s: Stat (ex. attack dmg, armor) ", "a: constant ", "b: constant ", "these constants can be defined here:").push("Skill Requirement Generator Config");
+        max_level = BUILDER.comment("Max level to generate (has to be less than or equal to reskillable config)").defineInRange("max_level", 32, 0, Integer.MAX_VALUE - 1);
+        damage_power = BUILDER.comment("Damage power").defineInRange("damage_power", 2F, 0, Float.MAX_VALUE);
+        damage_divisor = BUILDER.comment("Damage divisor").defineInRange("damage_divisor", 2F, 0, Float.MAX_VALUE);
+        armor_power = BUILDER.comment("Armor power").defineInRange("armor_power", 2F, 0, Float.MAX_VALUE);
+        armor_divisor = BUILDER.comment("Armor divisor").defineInRange("armor_divisor", 2F, 0, Float.MAX_VALUE);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
--- src/main/java/com/github/x3rmination/skillrequirementgenerator/mixin/ReSkillableConfigMixin.java
+++ src/main/java/com/github/x3rmination/skillrequirementgenerator/mixin/ReSkillableConfigMixin.java
@@ -1,15 +1,15 @@
 package com.github.x3rmination.skillrequirementgenerator.mixin;
 
 import com.github.x3rmination.skillrequirementgenerator.SkillReqConfig;
-import majik.rereskillable.Configuration;
-import majik.rereskillable.common.skills.Requirement;
-import majik.rereskillable.common.skills.Skill;
+import floris0106.rereskillablerereforked.common.Config;
+import floris0106.rereskillablerereforked.common.skills.Requirement;
+import floris0106.rereskillablerereforked.common.skills.Skill;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,41 +18,44 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
-@Mixin(Configuration.class)
+@Mixin(Config.class)
 public abstract class ReSkillableConfigMixin {
-
-    @Shadow(remap = false) @Final private static Map<String, Requirement[]> skillLocks;
-
+    @Shadow(remap = false) private static Config config;
     @Inject(method = "load", at = @At(value = "INVOKE_ASSIGN", target = "Ljava/util/List;iterator()Ljava/util/Iterator;", shift = At.Shift.AFTER), remap = false)
     private static void loadMixin(CallbackInfo ci) {
         ForgeRegistries.ITEMS.getEntries().forEach(entry -> {
-            if(entry.getValue().getRegistryName() != null) {
-                String registryName = String.valueOf(entry.getValue().getRegistryName());
-                ItemStack stack = entry.getValue().getDefaultInstance();
-                long damage = Math.round(stack.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().mapToDouble(AttributeModifier::getAmount).sum());
-                if (damage > 0) {
-                    int level = (int) Math.min((Math.pow(damage, SkillReqConfig.damage_power.get())) / SkillReqConfig.damage_divisor.get(), SkillReqConfig.max_level.get());
-                    if (level > 0) {
-                        Requirement[] requirements = new Requirement[1];
-                        requirements[0] = new Requirement(Skill.ATTACK, level);
-                        skillLocks.put(registryName, requirements);
+            ItemStack stack = entry.getValue().getDefaultInstance();
+            long damage = Math.round(stack.getAttributeModifiers(EquipmentSlotType.MAINHAND).get(Attributes.ATTACK_DAMAGE).stream().mapToDouble(AttributeModifier::getAmount).sum());
+            if (damage > 0) {
+                int level = (int) Math.min((Math.pow(damage, SkillReqConfig.damage_power.get())) / SkillReqConfig.damage_divisor.get(), SkillReqConfig.max_level.get());
+                if (level > 0) {
+                    Requirement[] requirements = new Requirement[1];
+                    requirements[0] = new Requirement(Skill.ATTACK, level);
+                    try {
+                        ((Map<ResourceLocation, Requirement[]>) config.getClass().getDeclaredField("skillLocks").get(config)).put(entry.getValue().getRegistryName(), requirements);
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        e.printStackTrace();
                     }
                 }
-                int armor = 0;
-                for (EquipmentSlotType type : EquipmentSlotType.values()) {
-                    if (type.getType().equals(EquipmentSlotType.Group.ARMOR)) {
-                        armor = (int) Math.round(stack.getAttributeModifiers(type).get(Attributes.ARMOR).stream().mapToDouble(AttributeModifier::getAmount).sum());
-                        if (armor > 0) {
-                            break;
-                        }
+            }
+            int armor = 0;
+            for (EquipmentSlotType type : EquipmentSlotType.values()) {
+                if (type.getType().equals(EquipmentSlotType.Group.ARMOR)) {
+                    armor = (int) Math.round(stack.getAttributeModifiers(type).get(Attributes.ARMOR).stream().mapToDouble(AttributeModifier::getAmount).sum());
+                    if (armor > 0) {
+                        break;
                     }
                 }
-                if (armor > 0) {
-                    int level = (int) Math.min((Math.pow(armor, SkillReqConfig.armor_power.get())) / SkillReqConfig.armor_divisor.get(), SkillReqConfig.max_level.get());
-                    if (level > 0) {
-                        Requirement[] requirements = new Requirement[1];
-                        requirements[0] = new Requirement(Skill.DEFENCE, level);
-                        skillLocks.put(registryName, requirements);
+            }
+            if (armor > 0) {
+                int level = (int) Math.min((Math.pow(armor, SkillReqConfig.armor_power.get())) / SkillReqConfig.armor_divisor.get(), SkillReqConfig.max_level.get());
+                if(level > 0) {
+                    Requirement[] requirements = new Requirement[1];
+                    requirements[0] = new Requirement(Skill.DEFENCE, level);
+                    try {
+                        ((Map<ResourceLocation, Requirement[]>) config.getClass().getDeclaredField("skillLocks").get(config)).put(entry.getValue().getRegistryName(), requirements);
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        e.printStackTrace();
                     }
                 }
             }
