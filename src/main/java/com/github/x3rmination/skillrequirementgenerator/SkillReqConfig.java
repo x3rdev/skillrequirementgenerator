d36bdf8c9d1661f55a278b7b8bb8a9831e07f311
X3RMINATION <nicolaeungur@gmail.com>
2023-01-29 20:30:11 -0800

    Changed function Fixed overwriting config Changed default values

--- src/main/java/com/github/x3rmination/skillrequirementgenerator/SkillReqConfig.java
+++ src/main/java/com/github/x3rmination/skillrequirementgenerator/SkillReqConfig.java
@@ -7,20 +7,22 @@ public class SkillReqConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.IntValue max_level;
-    public static final ForgeConfigSpec.DoubleValue damage_divisor;
-    public static final ForgeConfigSpec.DoubleValue damage_power;
-    public static final ForgeConfigSpec.DoubleValue armor_divisor;
-    public static final ForgeConfigSpec.DoubleValue armor_power;
+    public static final ForgeConfigSpec.DoubleValue damage_multiplier;
+    public static final ForgeConfigSpec.DoubleValue damage_addend;
+    public static final ForgeConfigSpec.DoubleValue damage_shift;
+    public static final ForgeConfigSpec.DoubleValue armor_multiplier;
+    public static final ForgeConfigSpec.DoubleValue armor_addend;
+    public static final ForgeConfigSpec.DoubleValue armor_shift;
 
 
     static {
-        BUILDER.comment("Formula for skill level: s^a / b", "s: Stat (ex. attack dmg, armor) ", "a: constant ", "b: constant ", "these constants can be defined here:").push("Skill Requirement Generator Config");
-        max_level = BUILDER.comment("Max level to generate (has to be less than or equal to reskillable config)").defineInRange("max_level", 32, 0, Integer.MAX_VALUE - 1);
-        damage_power = BUILDER.comment("Damage power").defineInRange("damage_power", 2F, 0, Float.MAX_VALUE);
-        damage_divisor = BUILDER.comment("Damage divisor").defineInRange("damage_divisor", 2F, 0, Float.MAX_VALUE);
-        armor_power = BUILDER.comment("Armor power").defineInRange("armor_power", 2F, 0, Float.MAX_VALUE);
-        armor_divisor = BUILDER.comment("Armor divisor").defineInRange("armor_divisor", 2F, 0, Float.MAX_VALUE);
+        BUILDER.comment("Formula for skill level: a * ln(x + b) + c", "s: Stat (ex. attack dmg, armor) ", "a: constant ", "b: constant ", "c: constant", "these constants can be defined here:", "Note: negative values inside the natural log will be set to 0").push("Skill Requirement Generator Config");
+        damage_multiplier = BUILDER.comment("Damage multiplier").defineInRange("damage_multiplier", 14F, 0, Float.MAX_VALUE);
+        damage_addend = BUILDER.comment("Damage addend").defineInRange("damage_addend", -3F, -Float.MAX_VALUE, Float.MAX_VALUE);
+        damage_shift = BUILDER.comment("Damage shift").defineInRange("damage_shift", -5F, -Float.MAX_VALUE, Float.MAX_VALUE);
+        armor_multiplier = BUILDER.comment("Armor multiplier").defineInRange("armor_multiplier", 14F, 0, Float.MAX_VALUE);
+        armor_addend = BUILDER.comment("Armor addend").defineInRange("armor_addend", -3F, -Float.MAX_VALUE, Float.MAX_VALUE);
+        armor_shift = BUILDER.comment("Armor addend").defineInRange("armor_shift", -5F, -Float.MAX_VALUE, Float.MAX_VALUE);
         BUILDER.pop();
         SPEC = BUILDER.build();
     }
